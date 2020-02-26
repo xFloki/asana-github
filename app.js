9480f189eb4e0633ec8e5f45198af03012140165
Alejandro Dietta <alexdietta@hotmail.com>
2020-02-26 03:18:24 +0100

    Manage pull requests

--- app.js
+++ app.js
@@ -11,15 +11,12 @@ app.post('/', function (req, res) {
   console.log(req.header('x-github-event'))
 
   const event = req.header('x-github-event')
-  const repoURL = req.body.head_commit.url 
-  let commitMessage = req.body.head_commit.message
-  let asanaRef = commitMessage.match( /(refs#\d+)/)
-  if (asanaRef) {
+  if (event === 'push') {
+    manageCommit(req.body);
+  } else if (event === 'pull_request') {
+    managePullRequest(req.body);
+  } 
 
-    let asanId = asanaRef[0].split('#')[1]
-    commentAsana(asanId, event, repoURL);
-  }
- 
   res.sendStatus(200);
  });
  
@@ -27,10 +24,34 @@ app.listen(port, function () {
  console.log('Listening on ' + port);
 });
 
-async function commentAsana(asanId, method, repoURL) {
-  try {
-    console.log('commentAsana')
+function manageCommit(body) {
+  const user = body.sender.login
+  const repoURL = body.head_commit.url 
+  const commitMessage = body.head_commit.message
+  const asanaRef = commitMessage.match( /(refs#\d+)/)
+  
+  if (asanaRef) {
+    const asanId = asanaRef[0].split('#')[1]
+    const msg = `Commit created by ${user}: ${repoURL}`
+    updateDescription(asanId, msg);
+  }
+}
 
+function managePullRequest(body) {
+  const user = body.pull_request.user.login
+  const action = body.action
+  const repoURL = body.pull_request.html_url
+  const pullRequestTitle = body.pull_request.title
+  let asanaRef = pullRequestTitle.match( /(refs#\d+)/)
+  if (asanaRef) {
+    const asanId = asanaRef[0].split('#')[1]
+    const msg = `Pull Request ${action} by ${user}: ${repoURL}`
+    updateDescription(asanId, msg);
+  }
+}
+
+async function updateDescription(asanId, msg) {
+  try {
     let me = await client.users.me();
     const userId = me.gid;
     const workspaceId = me.workspaces[0].gid;
@@ -39,20 +60,14 @@ async function commentAsana(asanId, method, repoURL) {
     let task = await client.tasks.findById(asanId)
     
     let description = {}
-    if (method === 'push') {
-      description.notes = 'Commit created by Alejandro: ' + repoURL + '\n' + '\n' + task.notes;
-    }
+    description.notes = msg + '\n' + '\n' + task.notes;
+  
     client.tasks.dispatchPut('/tasks/' + asanId, description)
 
   } catch(e) {
     console.log('Error al contactar con Asana')
     console.log(e.message)
   }
-  /*
-  task.dispatchPut('/tasks/' + asanId, comments) {
-
-  }
-  */
 }
 
 // /tasks/{task_gid}/stories COMENTARIOS https://developers.asana.com/docs/#get-a-story
