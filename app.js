d05ac5087964ca8b1cd03ce96bd8fadc0d4065c6
Alejandro Dietta <alexdietta@hotmail.com>
2020-02-26 01:21:48 +0100

    get asana task

--- app.js
+++ app.js
@@ -6,22 +6,38 @@ const port = process.env.PORT || 3000;
 let app = express();
 
 app.use( express.json());
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
-});
+
 app.post('/', function (req, res) {
   console.log(req.header('x-github-event'))
   console.log(req.headers)
   console.log(req.body)
-  commentAsana();
+
+  let asanaRef = asana1.match( /(refs#\d+)/)
+  if (asanaRef) {
+
+    let asanId = asanaRef[0].split('#')[1]
+    commentAsana(asanId);
+  }
+ 
   res.sendStatus(200);
  });
+ 
 app.listen(port, function () {
  console.log('Listening on ' + port);
 });
 
-function commentAsana() {
+async function commentAsana(asanId) {
   client.users.me().then(function(me) {
     console.log(me);
   });
+
+  let me = await client.users.me();
+  const userId = user.gid;
+  const workspaceId = user.workspaces[0].gid;
+
+  let task = await client.tasks.findById('asanId')
+
+  console.log(task.id)
+  console.log(task.assignee.name)
+  
 }
